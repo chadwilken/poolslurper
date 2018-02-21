136fb97a9e719c3622898691d7c1e49f4b64389d
Adam Piontek <adam@73k.us>
2018-02-20 22:06:51 -0500

    first commit

---
+++ slurp.py
@@ -0,0 +1,355 @@
+import os
+import csv
+import time
+from datetime import datetime
+import json
+import requests
+
+
+# quick app to grab mining pool data for several pools and save in csv files
+# process:
+#   1) record "now"
+#   2) poll pool api data (in threads?)
+#   3) organize data for new row of csv file
+#   4) write csv file
+class Pool(object):
+    def __init__(self,
+                    name=None,
+                    apiurl=None,
+                    apiparams=dict(),
+                    csvfile=None,
+                    divisors=dict()
+                ):
+        self.name = name
+        self.apiurl = apiurl
+        self.apiparams = apiparams
+        self.csvfile = csvfile
+        self.response = ""
+        self.algorithms = {}
+        self.divisors = divisors
+        self.timeouts = 0
+        self.failures = 0
+
+    def __repr__(self):
+        return "Pool(" + \
+                "\'name\'=\'{}\', ".format(self.name) + \
+                "\'apiurl\'=\'{}\', ".format(self.apiurl) + \
+                "\'apiparams\'=\'{}\', ".format(self.apiparams) + \
+                "\'algorithms\'=\'{}\', ".format(self.algorithms) + \
+                "\'divisors\'=\'{}\', ".format(self.divisors) + \
+                "\'timeouts\'=\'{}\', ".format(self.timeouts) + \
+                "\'failures\'=\'{}\'".format(self.failures) + \
+                ")"
+
+    def __str__(self):
+        return self.__dict__
+
+    def poll_api(self):
+        sleeptime=5
+        self.timeouts = 0
+        r = None
+        while self.timeouts < 3:
+            try:
+                if self.apiparams:
+                    r = requests.get(url=self.apiurl, params=self.apiparams)
+                else:
+                    r = requests.get(url=self.apiurl)
+                r.raise_for_status()
+            except requests.exceptions.Timeout:
+                self.timeouts += 1
+                time.sleep(sleeptime)
+                sleeptime += 3
+                continue
+            except requests.exceptions.HTTPError as err:
+                self.timeouts = 404
+            except requests.exceptions.RequestException as e:
+                self.timeouts = 666
+            break
+        if self.timeouts >= 3 or r is None or r.text is None or "{" not in r.text:
+            self.failures += 1
+            return False
+        data = json.loads(r.text)
+        if data is None or data is False:
+            self.failures += 1
+            return False
+        self.response = data
+        return True
+
+    def update_algorithms(self, valid_algos):
+        self.algorithms = {}
+        if not self.poll_api():
+            return
+        else:
+            for key in self.response:
+                if key in valid_algos:
+                    self.algorithms[valid_algos[key]]=self.response[key]['estimate_current']
+
+    def get_algorithms(self):
+        return self.algorithms
+
+    def get_name(self):
+        return self.name
+
+    def get_algorithm_divisor(self, algorithm):
+        divisor = 0
+        if algorithm in self.divisors:
+            divisor = self.divisors[algorithm]
+        else:
+            divisor = self.divisors['default']
+        return divisor
+
+    def get_csv_string(self, new_timestamp, algo_map):
+        new_row = [new_timestamp]
+        for i in range(len(algo_map)):
+            if algo_map[i] in self.algorithms:
+                new_row.append(str(self.algorithms[algo_map[i]]))
+            else:
+                new_row.append('')
+        return new_row
+
+    def append_row_to_csv(self, new_row):
+        with open(self.csvfile, 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow(new_row)
+
+
+
+
+class MiningPoolHub(Pool):
+    def __init__(self,
+                    name=None,
+                    apiurl=None,
+                    apiparams=None,
+                    csvfile=None,
+                    divisors=None
+                ):
+        Pool.__init__(self, name, apiurl, apiparams, csvfile, divisors)
+
+    def update_algorithms(self, valid_algos):
+        self.algorithms = {}
+        poll_result = self.poll_api()
+        if not poll_result or not self.response['success']:
+            return
+        else:
+            for obj in self.response['return']:
+                obj['algo'] = obj['algo'].lower()
+                if obj['algo'] in valid_algos:
+                    self.algorithms[valid_algos[obj['algo']]] = obj['profit']
+
+
+
+
+class Nicehash(Pool):
+    def __init__(self,
+                    name=None,
+                    apiurl=None,
+                    apiparams=None,
+                    csvfile=None,
+                    divisors=None
+                ):
+        Pool.__init__(self, name, apiurl, apiparams, csvfile, divisors)
+
+    def update_algorithms(self, valid_algos):
+        self.algorithms = {}
+        poll_result = self.poll_api()
+        if poll_result is False or \
+				'simplemultialgo' not in str(self.response) or \
+				self.response['result'] is False or \
+				self.response['result']['simplemultialgo'] is False:
+            return
+        else:
+            for obj in self.response['result']['simplemultialgo']:
+                if obj['name'] in valid_algos:
+                    self.algorithms[valid_algos[obj['name']]]=obj['paying']
+
+
+
+
+
+def main():
+    script_home = os.path.split(os.path.abspath(__file__))[0]
+
+    valid_algos = {
+        "bitcore": "bitcore",
+        "blake": "blakecoin",
+        "blake2s": "blake2s",
+        "blakecoin": "blakecoin",
+        "blakevanilla": "blakevanilla",
+        "c11": "c11",
+        "cryptonight": "cryptonight",
+        "daggerhashimoto": "ethash",
+        "darkcoinmod": "x11",
+        "decred": "decred",
+        "equihash": "equihash",
+        "eth": "ethash",
+        "ethash": "ethash",
+        "groestl": "groestl",
+        "groestlcoin": "groestl",
+        "hmq1725": "hmq1725",
+        "hsr": "hsr",
+        "jha": "jha",
+        "keccak": "keccak",
+        "lbry": "lbry",
+        "lyra2re2": "lyra2v2",
+        "lyra2rev2": "lyra2v2",
+        "lyra2v2": "lyra2v2",
+        "lyra2z": "lyra2z",
+        "maxcoin": "keccak",
+        "myrgr": "myriadgroestl",
+        "myr-gr": "myriadgroestl",
+        "myriadcoingroestl": "myriadgroestl",
+        "myriadgroestl": "myriadgroestl",
+        "neoscrypt": "neoscrypt",
+        "nist5": "nist5",
+        "pascal": "pascal",
+        "phi": "phi",
+        "poly": "polytimos",
+        "polytimos": "polytimos",
+        "quark": "quark",
+        "quarkcoin": "quark",
+        "qubit": "qubit",
+        "qubitcoin": "qubit",
+        "scrypt": "scrypt",
+        "sha256": "sha256",
+        "sib": "sib",
+        "sibcoinmod": "sib",
+        "sigt": "skunk",
+        "skein": "skein",
+        "skeincoin": "skein",
+        "skunk": "skunk",
+        "timetravel": "timetravel",
+        "tribus": "tribus",
+        "vanilla": "blakevanilla",
+        "whirlpoolx": "whirlpoolx",
+        "veltor": "veltor",
+        "x11": "x11",
+        "x11gost": "sib",
+        "x11evo": "x11evo",
+        "x17": "x17",
+        "xevan": "xevan",
+        "xmr": "cryptonight",
+        "yescrypt": "yescrypt",
+        "zec": "equihash",
+        "zuikkis": "scrypt"
+    }
+
+
+    algo_map = ["bitcore", "blake2s", "blakecoin", "c11", "cryptonight", \
+                "decred", "equihash", "ethash", "groestl", "hmq1725", \
+                "hsr", "keccak", "lbry", "lyra2v2", "lyra2z", "m7m", \
+                "myr-gr", "neoscrypt", "nist5", "pascal", "phi", \
+                "polytimos", "quark", "qubit", "scrypt", "sha256", \
+                "sia", "sib", "skein", "skunk", "timetravel", "tribus", \
+                "whirlpoolx", "x11", "x11evo", "x11gost", "x13", "x14", \
+                "x15", "x17", "xevan", "yescrypt", "yescryptR16"]
+
+
+    pools = []
+
+    # ahashpool
+    pools.append(Pool(
+            name='ahashpool',
+            apiurl='https://www.ahashpool.com/api/status/',
+            csvfile=os.path.join(os.path.split(os.path.abspath(__file__))[0], 'csvdata', 'ahashpool.csv'),
+            divisors=dict(
+                default=1000000.0,
+                equihash=1000.0,
+                blake2s=1000000000.0,
+                blakecoin=1000000000.0,
+                decred=1000000000.0
+            )
+        )
+    )
+
+    # blazepool
+    pools.append(Pool(
+            name='blazepool',
+            apiurl='http://api.blazepool.com/status',
+            csvfile=os.path.join(os.path.split(os.path.abspath(__file__))[0], 'csvdata', 'blazepool.csv'),
+            divisors=dict(
+                default=1000000.0,
+                equihash=1000.0,
+                blake2s=1000000000.0,
+                blakecoin=1000000000.0,
+                keccak=1000000000.0,
+                decred=1000000000.0
+            )
+        )
+    )
+
+    # zergpool
+    pools.append(Pool(
+            name='zergpool',
+            apiurl='http://api.zergpool.com:8080/api/status',
+            csvfile=os.path.join(os.path.split(os.path.abspath(__file__))[0], 'csvdata', 'zergpool.csv'),
+            divisors=dict(
+                default=1000000.0,
+                equihash=1000.0,
+                blake2s=1000000000.0,
+                blakecoin=1000000000.0,
+                quark=1000000000.0,
+                keccak=1000000000.0,
+                decred=1000000000.0
+            )
+        )
+    )
+
+    # zpool
+    pools.append(Pool(
+            name='zpool',
+            apiurl='http://www.zpool.ca/api/status/',
+            csvfile=os.path.join(os.path.split(os.path.abspath(__file__))[0], 'csvdata', 'zpool.csv'),
+            divisors=dict(
+                default=1000000.0,
+                equihash=1000.0,
+                blake2s=1000000000.0,
+                blakecoin=1000000000.0,
+                decred=1000000000.0,
+                x11=1000000000.0,
+                quark=1000000000.0,
+                qubit=1000000000.0,
+                scrypt=1000000000.0,
+                keccak=1000000000.0
+            )
+        )
+    )
+
+    # miningpoolhub
+    pools.append(MiningPoolHub(
+            name='miningpoolhub',
+            apiurl='https://miningpoolhub.com/index.php',
+            apiparams=dict(
+                page='api',
+                action='getautoswitchingandprofitsstatistics'
+            ),
+            csvfile=os.path.join(os.path.split(os.path.abspath(__file__))[0], 'csvdata', 'miningpoolhub.csv'),
+            divisors=dict(default=1000000000.0)
+        )
+    )
+
+    # nicehash
+    pools.append(Nicehash(
+            name='nicehash',
+            apiurl='https://api.nicehash.com/api',
+            apiparams=dict(
+                method='simplemultialgo.info'
+            ),
+            csvfile=os.path.join(os.path.split(os.path.abspath(__file__))[0], 'csvdata', 'nicehash.csv'),
+            divisors=dict(default=1000000000.0)
+        )
+    )
+
+
+    # so that should give us a list of the pools as class objects
+    while (1):
+        new_datetime = datetime.now()
+        new_timestamp = new_datetime.strftime('%x %X')
+        for pool in pools:
+            pool.update_algorithms(valid_algos)
+            pool.append_row_to_csv(pool.get_csv_string(new_timestamp, algo_map))
+        time.sleep(60)
+
+
+
+if __name__ == '__main__':
+    main()
